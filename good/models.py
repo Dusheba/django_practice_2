c4a6f407f3e3b02f80e993a87465b01edddf2866
Михаил Рыбкин <40176967+MikerStudio@users.noreply.github.com>
2021-03-26 21:18:45 +0300

    Update models.py

--- good/models.py
+++ good/models.py
@@ -2,6 +2,14 @@ from django.db import models
 
 
 class Good(models.Model):
-    name = models.CharField(max_length=100)
-    image = models.ImageField(verbose_name="Картинка")
-    price = models.IntegerField(null=True, blank=True)
+    name = models.CharField(
+        max_length=100
+    )
+    image = models.ImageField(
+        upload_to="images/"
+        verbose_name="Картинка"
+    )
+    price = models.IntegerField(
+        null=True,
+        blank=True
+    )
