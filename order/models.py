c87ce2ff9f0795703875938a4447d1df01ed4c59
Михаил Рыбкин <40176967+MikerStudio@users.noreply.github.com>
2021-03-26 21:13:33 +0300

    Update models.py

--- order/models.py
+++ order/models.py
@@ -3,9 +3,16 @@ from good.models import Good
 
 
 class Order(models.Model):
-    name = models.CharField(max_length=100)
-    date_created = models.DateTimeField(auto_now_add=True)
-    id_goods = models.ForeignKey(Good, on_delete=models.CASCADE)
+    name = models.CharField(
+        max_length=100
+    )
+    date_created = models.DateTimeField(
+        auto_now_add=True
+    )
+    id_goods = models.ForeignKey(
+        Good, 
+        on_delete=models.CASCADE
+    )
 
 
 def __str__(self):
